a0b619b275f43db0bf542de63b986b117a5b5515
Gaspar Juiz Gargiulo <gaspi@Mac-mini-de-Gaspar.local>
2022-06-29 12:04:56 -0300

    Tarea02

---
+++ 02 - Variables y Tipos de Datos/Homework_02.py
@@ -0,0 +1,121 @@
+# 1)
+
+from re import I
+
+
+juan = int(5)
+print(juan)
+
+# 2)
+
+print(type(8.5))
+
+# 3)
+
+print(type(juan))
+
+# 4)
+
+nombre = "Gaspar"
+
+# 5)
+
+complejo = complex(5, 2)
+print(complejo)
+
+# 6)
+
+print(type(complejo))
+
+# 7)
+
+pi = 3.1416
+
+# 8)
+
+TrueBool = True
+TrueString = "True"
+# No es el mismo tipo de dato
+
+# 9)
+
+print(type(TrueBool))
+print(type(TrueString))
+
+# 10)
+sumando = 10 + 10.5
+print(sumando)
+
+# 11)
+
+ComplexSum = complejo + complex(1, 1)
+print(ComplexSum)
+
+# 12)
+
+sumando2 = 2 + complejo
+print(sumando2)
+
+# 13)
+
+print(2*2)
+
+# 14)
+
+print(2**8)
+
+# 15)
+
+cociente = float(27/4)
+print(cociente)
+
+# 16)
+
+
+cociente = int(cociente)
+print(cociente)  
+
+
+# 17)
+
+resto = 27 - 4*6
+print(resto)
+
+# 18)
+
+print(resto + cociente*4)
+
+# 19)
+
+juan = "juan"
+numero = "1"
+concatenar = juan + numero
+print(concatenar)
+
+# 20)
+
+juan = "2"
+marcos = 2
+if juan == marcos:
+    print("se rompio todo")
+else:
+    print("son cosas distintas")
+
+# 21)
+
+juan = int(juan)
+
+if juan == marcos:
+    print("ahora si")
+else:
+    print("son cosas distintas")
+
+# 22)
+
+''' por la coma '''
+
+# 23)
+
+valor = 3
+valor -= 1
+print(valor)
